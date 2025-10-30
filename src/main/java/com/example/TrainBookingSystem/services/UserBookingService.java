6667b7b9834447644201da7987bc1f0d8008944e
Hyouka45 <khewang212@gmail.com>
2025-10-30 19:41:14 +0545

    data loaded

--- src/main/java/com/example/TrainBookingSystem/services/UserBookingService.java
+++ src/main/java/com/example/TrainBookingSystem/services/UserBookingService.java
@@ -1,4 +1,30 @@
 package com.example.TrainBookingSystem.services;
 
+import com.example.TrainBookingSystem.entities.User;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class UserBookingService {
+    private User user;
+
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+
+        });
+
+    }
+
 }
